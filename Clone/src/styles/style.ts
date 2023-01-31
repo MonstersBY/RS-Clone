fb649958e72b099a47d06e66d3331277ff025c78
YanaPronko <YaniaR@tut.by>
2023-01-31 16:56:57 +0300

    feat: change config, start html

---
+++ Clone/src/styles/style.ts
@@ -0,0 +1,8 @@
+import "./resetStyles.scss";
+import "./constants.scss";
+import "./mixins.scss";
+import "./placeholder.scss";
+import "./base.scss";
+import "./logo.scss";
+import "./header.scss";
+import "./footer.scss";
--- Clone/webpack.config.js
+++ Clone/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -22,6 +24,16 @@ const config = {
     }),
 
     new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "**/*",
+          context: path.resolve(__dirname, "src", "assets"),
+          to: "./assets",
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
@@ -31,8 +43,8 @@ const config = {
         exclude: ["/node_modules/"],
       },
       {
-        test: /\.css$/i,
-        use: [stylesHandler, "css-loader"],
+        test: /\.(sa|sc|c)ss$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
