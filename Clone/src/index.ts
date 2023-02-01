63aadb395b215dd19c452759b1c49547fc2726f0
Arseny Fedorchenko <Arantola@mail.ru>
2023-02-01 17:30:20 +0300

    feat: add simple helper

--- Clone/src/index.ts
+++ Clone/src/index.ts
@@ -3,4 +3,12 @@ import "./styles/main.scss";
 
 import helperNode from "./modules/helper/helper";
 document.body.insertAdjacentHTML("beforeend", helperNode);
-const helper = document.querySelector(".hel")
+const helperWrapper = document.querySelector(".helper__wrapper");
+const helperBtn = document.querySelector(".helper___button");
+const helperBody = document.querySelector(".helper__body");
+
+helperWrapper?.addEventListener("click", (e) => {
+  if (e.target === helperBtn) {
+    helperBody?.classList.toggle("hidden");
+  }
+})
