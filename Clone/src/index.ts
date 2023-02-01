34bf6fd5e04d2634ad533c882f98a417e95d4297
Arseny Fedorchenko <Arantola@mail.ru>
2023-02-01 15:37:57 +0300

    refactor: change structure of styles, add basis of helper

--- Clone/src/index.ts
+++ Clone/src/index.ts
@@ -1,1 +1,6 @@
-import "./styles/style";
+import "./styles/main.scss";
+
+
+import helperNode from "./modules/helper/helper";
+document.body.insertAdjacentHTML("beforeend", helperNode);
+const helper = document.querySelector(".hel")
