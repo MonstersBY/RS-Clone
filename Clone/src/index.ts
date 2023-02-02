e37eecf3f2bca53411028fde388dd1dcbdffb152
Arseny Fedorchenko <Arantola@mail.ru>
2023-02-02 11:55:01 +0300

    feat: add event listener to call helper

--- Clone/src/index.ts
+++ Clone/src/index.ts
@@ -10,5 +10,10 @@ const helperBody = document.querySelector(".helper__body");
 helperWrapper?.addEventListener("click", (e) => {
   if (e.target === helperBtn) {
     helperBody?.classList.toggle("hidden");
+    helperWrapper.classList.toggle("nobackground");
+  }
+  if (e.target === helperWrapper) {
+    helperBody?.classList.add("hidden");
+    helperWrapper.classList.add("nobackground");
   }
 })
