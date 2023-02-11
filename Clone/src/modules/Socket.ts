bf4c644af2e76f964deb326e99dcc647a5ad6ce9
Alex Romanchenko <monsters.alex0@gmail.com>
2023-02-11 18:38:03 +0300

    fix: del withCredentials

--- Clone/src/modules/Socket.ts
+++ Clone/src/modules/Socket.ts
@@ -2,10 +2,9 @@ import {io} from "socket.io-client"
 
 // const socket = io('http://localhost:3000/')
 const socket = io('https://example-pdrb.onrender.com', {
-    withCredentials: true,
     extraHeaders: {
       "my-custom-header": "abcd"
     }
-  })
+})
 
 export default socket
