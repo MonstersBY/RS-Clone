6bde0fcaebf28254fb449e16c332ea4546605e63
Alex Romanchenko <monsters.alex0@gmail.com>
2023-02-11 18:50:22 +0300

    fix: del socket headers

--- Clone/src/modules/Socket.ts
+++ Clone/src/modules/Socket.ts
@@ -1,10 +1,10 @@
 import {io} from "socket.io-client"
 
 // const socket = io('http://localhost:3000/')
-const socket = io('https://example-pdrb.onrender.com', {
-    extraHeaders: {
-      "my-custom-header": "abcd"
-    }
-})
-
+const socket = io('https://example-pdrb.onrender.com')
+// , {
+//   extraHeaders: {
+//     "my-custom-header": "abcd"
+//   }
+// }
 export default socket
