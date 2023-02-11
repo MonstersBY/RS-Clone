0ec6c8d0aa6e2faf32f91a7a51f5ec53f591b3f3
Alex Romanchenko <monsters.alex0@gmail.com>
2023-02-11 16:56:56 +0300

    fix: server addres

--- Clone/src/modules/Socket.ts
+++ Clone/src/modules/Socket.ts
@@ -1,6 +1,11 @@
 import {io} from "socket.io-client"
 
 // const socket = io('http://localhost:3000/')
-const socket = io('https://example-pdrb.onrender.com')
+const socket = io('https://example-pdrb.onrender.com', {
+    withCredentials: true,
+    extraHeaders: {
+      "my-custom-header": "abcd"
+    }
+  })
 
 export default socket
