3da67082df59fcd16eca059710e0f09acbaae224
Alex Romanchenko <monsters.alex0@gmail.com>
2023-02-15 11:32:56 +0300

    fix: add comma to socket

--- Clone/src/modules/Socket.ts
+++ Clone/src/modules/Socket.ts
@@ -2,7 +2,7 @@ import {io} from "socket.io-client"
 
 // const socket = io('http://localhost:3000/')
 
-const socket = io('https://example-pdrb.onrender.com'{
+const socket = io('https://example-pdrb.onrender.com',{
   extraHeaders: {
     "my-custom-header": "colonist"
   }
